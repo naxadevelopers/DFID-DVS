52340f5c06508450011fb962a637fe43e957c97c
raeesaroj <raesaroj16@gmail.com>
2018-04-30 17:20:13 +0545

    update static settings for production

--- dfid/urls.py
+++ dfid/urls.py
@@ -27,4 +27,4 @@ urlpatterns = [
     path('core/api/docs/', schema_view)
 
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
